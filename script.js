ad77b3ec8a0a77aa3af7457d6bcd88c3b626243c
cedric43-2718 <65771883+cedric43-2718@users.noreply.github.com>
2020-11-21 18:35:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ console.log(sticky);
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    document.getElementById("site-title").style.display = "none";
   } else {
     header.classList.remove("sticky");
+    document.getElementById("site-title").style.display = "inline";
   }
 }
 
